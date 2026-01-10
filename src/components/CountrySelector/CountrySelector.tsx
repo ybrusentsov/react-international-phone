00aeaa8853b46d967e8036d37e24272b96f0bc2a
ybrusentsov <yuriybrusentsov@gmail.com>
2026-01-11 01:01:24 +0200

    perf: optimize rerenders in usePhoneInput, wrap CountrySelector with React.memo

--- src/components/CountrySelector/CountrySelector.tsx
+++ src/components/CountrySelector/CountrySelector.tsx
@@ -61,145 +61,148 @@ export interface CountrySelectorProps extends CountrySelectorStyleProps {
   }) => React.ReactNode;
 }
 
-export const CountrySelector: React.FC<CountrySelectorProps> = ({
-  selectedCountry,
-  onSelect,
-  disabled,
-  hideDropdown,
-  countries = defaultCountries,
-  preferredCountries = [],
-  flags,
-  renderButtonWrapper,
-  ...styleProps
-}) => {
-  const [showDropdown, setShowDropdown] = useState(false);
+export const CountrySelector: React.FC<CountrySelectorProps> = React.memo(
+  ({
+    selectedCountry,
+    onSelect,
+    disabled,
+    hideDropdown,
+    countries = defaultCountries,
+    preferredCountries = [],
+    flags,
+    renderButtonWrapper,
+    ...styleProps
+  }) => {
+    const [showDropdown, setShowDropdown] = useState(false);
 
-  const fullSelectedCountry = useMemo(() => {
-    if (!selectedCountry) return undefined;
-    return getCountry({
-      value: selectedCountry,
-      field: 'iso2',
-      countries: countries,
-    });
-  }, [countries, selectedCountry]);
+    const fullSelectedCountry = useMemo(() => {
+      if (!selectedCountry) return undefined;
+      return getCountry({
+        value: selectedCountry,
+        field: 'iso2',
+        countries: countries,
+      });
+    }, [countries, selectedCountry]);
 
-  const countrySelectorRef = useRef<HTMLDivElement>(null);
+    const countrySelectorRef = useRef<HTMLDivElement>(null);
 
-  const handleKeyDown = (e: React.KeyboardEvent) => {
-    if (!e.key) return;
+    const handleKeyDown = (e: React.KeyboardEvent) => {
+      if (!e.key) return;
 
-    if (['ArrowUp', 'ArrowDown'].includes(e.key)) {
-      e.preventDefault();
-      setShowDropdown(true);
-    }
-  };
-
-  const renderSelectorButton = () => {
-    const rootProps: RenderButtonWrapperRootProps = {
-      title: fullSelectedCountry?.name,
-      onClick: () => setShowDropdown((v) => !v),
-      // Need this to close dropdown on selector button click
-      // https://stackoverflow.com/a/28963938
-      onMouseDown: (e) => e.preventDefault(),
-      onKeyDown: handleKeyDown,
-      disabled: hideDropdown || disabled,
-      role: 'combobox',
-      'aria-label': 'Country selector',
-      'aria-haspopup': 'listbox',
-      'aria-expanded': showDropdown,
+      if (['ArrowUp', 'ArrowDown'].includes(e.key)) {
+        e.preventDefault();
+        setShowDropdown(true);
+      }
     };
 
-    const buttonContent = (
-      <div
-        className={buildClassNames({
-          addPrefix: ['country-selector-button__button-content'],
-          rawClassNames: [styleProps.buttonContentWrapperClassName],
-        })}
-        style={styleProps.buttonContentWrapperStyle}
-      >
-        <FlagImage
-          iso2={selectedCountry}
-          src={flags?.find((f) => f.iso2 === selectedCountry)?.src}
+    const renderSelectorButton = () => {
+      const rootProps: RenderButtonWrapperRootProps = {
+        title: fullSelectedCountry?.name,
+        onClick: () => setShowDropdown((v) => !v),
+        // Need this to close dropdown on selector button click
+        // https://stackoverflow.com/a/28963938
+        onMouseDown: (e) => e.preventDefault(),
+        onKeyDown: handleKeyDown,
+        disabled: hideDropdown || disabled,
+        role: 'combobox',
+        'aria-label': 'Country selector',
+        'aria-haspopup': 'listbox',
+        'aria-expanded': showDropdown,
+      };
+
+      const buttonContent = (
+        <div
           className={buildClassNames({
-            addPrefix: [
-              'country-selector-button__flag-emoji',
-              disabled && 'country-selector-button__flag-emoji--disabled',
-            ],
-            rawClassNames: [styleProps.flagClassName],
+            addPrefix: ['country-selector-button__button-content'],
+            rawClassNames: [styleProps.buttonContentWrapperClassName],
           })}
-          style={{
-            visibility: selectedCountry ? 'visible' : 'hidden',
-            ...styleProps.flagStyle,
-          }}
-        />
-        {!hideDropdown && (
-          <div
+          style={styleProps.buttonContentWrapperStyle}
+        >
+          <FlagImage
+            iso2={selectedCountry}
+            src={flags?.find((f) => f.iso2 === selectedCountry)?.src}
             className={buildClassNames({
               addPrefix: [
-                'country-selector-button__dropdown-arrow',
-                disabled && 'country-selector-button__dropdown-arrow--disabled',
-                showDropdown &&
-                  'country-selector-button__dropdown-arrow--active',
+                'country-selector-button__flag-emoji',
+                disabled && 'country-selector-button__flag-emoji--disabled',
               ],
-              rawClassNames: [styleProps.dropdownArrowClassName],
+              rawClassNames: [styleProps.flagClassName],
             })}
-            style={styleProps.dropdownArrowStyle}
+            style={{
+              visibility: selectedCountry ? 'visible' : 'hidden',
+              ...styleProps.flagStyle,
+            }}
           />
-        )}
+          {!hideDropdown && (
+            <div
+              className={buildClassNames({
+                addPrefix: [
+                  'country-selector-button__dropdown-arrow',
+                  disabled &&
+                    'country-selector-button__dropdown-arrow--disabled',
+                  showDropdown &&
+                    'country-selector-button__dropdown-arrow--active',
+                ],
+                rawClassNames: [styleProps.dropdownArrowClassName],
+              })}
+              style={styleProps.dropdownArrowStyle}
+            />
+          )}
+        </div>
+      );
+      if (renderButtonWrapper) {
+        return renderButtonWrapper({
+          children: buttonContent,
+          rootProps: rootProps,
+        });
+      }
+      return (
+        <button
+          {...rootProps}
+          type="button"
+          className={buildClassNames({
+            addPrefix: [
+              'country-selector-button',
+              showDropdown && 'country-selector-button--active',
+              disabled && 'country-selector-button--disabled',
+              hideDropdown && 'country-selector-button--hide-dropdown',
+            ],
+            rawClassNames: [styleProps.buttonClassName],
+          })}
+          data-country={selectedCountry}
+          style={styleProps.buttonStyle}
+        >
+          {buttonContent}
+        </button>
+      );
+    };
+
+    return (
+      <div
+        className={buildClassNames({
+          addPrefix: ['country-selector'],
+          rawClassNames: [styleProps.className],
+        })}
+        style={styleProps.style}
+        ref={countrySelectorRef}
+      >
+        {renderSelectorButton()}
+        <CountrySelectorDropdown
+          show={showDropdown}
+          countries={countries}
+          preferredCountries={preferredCountries}
+          flags={flags}
+          onSelect={(country) => {
+            setShowDropdown(false);
+            onSelect?.(country);
+          }}
+          selectedCountry={selectedCountry}
+          onClose={() => {
+            setShowDropdown(false);
+          }}
+          {...styleProps.dropdownStyleProps}
+        />
       </div>
     );
-    if (renderButtonWrapper) {
-      return renderButtonWrapper({
-        children: buttonContent,
-        rootProps: rootProps,
-      });
-    }
-    return (
-      <button
-        {...rootProps}
-        type="button"
-        className={buildClassNames({
-          addPrefix: [
-            'country-selector-button',
-            showDropdown && 'country-selector-button--active',
-            disabled && 'country-selector-button--disabled',
-            hideDropdown && 'country-selector-button--hide-dropdown',
-          ],
-          rawClassNames: [styleProps.buttonClassName],
-        })}
-        data-country={selectedCountry}
-        style={styleProps.buttonStyle}
-      >
-        {buttonContent}
-      </button>
-    );
-  };
-
-  return (
-    <div
-      className={buildClassNames({
-        addPrefix: ['country-selector'],
-        rawClassNames: [styleProps.className],
-      })}
-      style={styleProps.style}
-      ref={countrySelectorRef}
-    >
-      {renderSelectorButton()}
-      <CountrySelectorDropdown
-        show={showDropdown}
-        countries={countries}
-        preferredCountries={preferredCountries}
-        flags={flags}
-        onSelect={(country) => {
-          setShowDropdown(false);
-          onSelect?.(country);
-        }}
-        selectedCountry={selectedCountry}
-        onClose={() => {
-          setShowDropdown(false);
-        }}
-        {...styleProps.dropdownStyleProps}
-      />
-    </div>
-  );
-};
+  },
+);
